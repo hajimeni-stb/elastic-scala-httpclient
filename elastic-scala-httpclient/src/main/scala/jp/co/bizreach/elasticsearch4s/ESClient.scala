5a0b30a9d5a689c7eb00cafefcfc1b5c93ccb911
Naoki Takezoe <takezoe@gmail.com>
2017-10-12 11:46:22 +0900

    Fix error extraction in bulk request

--- elastic-scala-httpclient/src/main/scala/jp/co/bizreach/elasticsearch4s/ESClient.scala
+++ elastic-scala-httpclient/src/main/scala/jp/co/bizreach/elasticsearch4s/ESClient.scala
@@ -410,7 +410,7 @@ class ESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAvailab
   def bulk[T](actions: Seq[BulkAction]): Either[Map[String, Any], Map[String, Any]] = {
     val resultJson = HttpUtils.post(httpClient, s"${url}/_bulk", actions.map(_.jsonString).mkString("", "\n", "\n"))
     val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
-    map.get("error").map { case message: String => Left(map) }.getOrElse(Right(map))
+    map.get("errors").collect { case true => Left(map) }.getOrElse(Right(map))
   }
 
 }
