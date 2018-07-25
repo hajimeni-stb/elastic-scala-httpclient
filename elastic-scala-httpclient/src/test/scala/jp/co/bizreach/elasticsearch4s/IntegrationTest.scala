78eb95d9dd1ef310f557059f288e3d53a46b1b08
安藤 絵里花 <erika.ando+github@bizreach.co.jp>
2018-07-25 09:52:02 +0900

    add test

--- elastic-scala-httpclient/src/test/scala/jp/co/bizreach/elasticsearch4s/IntegrationTest.scala
+++ elastic-scala-httpclient/src/test/scala/jp/co/bizreach/elasticsearch4s/IntegrationTest.scala
@@ -265,6 +265,16 @@ class IntegrationTest extends FunSuite with BeforeAndAfter with BeforeAndAfterAl
       }
     }
     assert(count5 == 99)
+
+    // Scroll by Json search
+    val jsonRequest = """{"query": {"match_all": {}}, "size": 1}"""
+    val sum2 = client.scrollJson[Blog, Int](config, jsonRequest){ builder =>
+      builder.query(matchPhraseQuery("subject", "Hello"))
+    }{ case (id, blog) =>
+      assert(blog.content == "This is a first registration test!")
+      1
+    }.sum
+    assert(sum2 == 99)
   }
 
   test("noFields"){
