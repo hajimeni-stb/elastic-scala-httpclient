6e2f4c325e80cf1acfbc024cc7eea766e81a2fb2
shimamoto <chibochibo@gmail.com>
2019-05-30 13:29:41 +0900

    Support elasticsearch 6.7

--- elastic-scala-httpclient/src/main/scala/jp/co/bizreach/elasticsearch4s/AsyncESClient.scala
+++ elastic-scala-httpclient/src/main/scala/jp/co/bizreach/elasticsearch4s/AsyncESClient.scala
@@ -4,9 +4,9 @@ import ESUtils._
 import org.slf4j.LoggerFactory
 
 import scala.reflect.ClassTag
-import com.ning.http.client.{AsyncHttpClient, AsyncHttpClientConfig}
 import jp.co.bizreach.elasticsearch4s.retry.{FixedBackOff, RetryConfig, FutureRetryManager}
-import org.codelibs.elasticsearch.querybuilders.SearchDslBuilder
+import org.asynchttpclient.{AsyncHttpClient, AsyncHttpClientConfig}
+import org.elasticsearch.search.builder.SearchSourceBuilder
 
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -63,11 +63,11 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
 
   val logger = LoggerFactory.getLogger(classOf[AsyncESClient])
 
-  def searchAsync(config: ESConfig)(f: SearchDslBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = {
+  def searchAsync(config: ESConfig)(f: SearchSourceBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = {
     logger.debug("******** ESConfig:" + config.toString)
-    val builder = SearchDslBuilder.builder()
+    val builder = new SearchSourceBuilder()
     f(builder)
-    val json = builder.build()
+    val json = builder.toString
     logger.debug(s"searchRequest:${json}")
 
     val future = HttpUtils.postAsync(httpClient, config.preferenceUrl(url, "_search"), json)
@@ -77,7 +77,7 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def searchAllAsync(config: ESConfig)(f: SearchDslBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = {
+  def searchAllAsync(config: ESConfig)(f: SearchSourceBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = {
     countAsync(config)(f).flatMap {
       case Left(x)  => Future(Left(x))
       case Right(x) => {
@@ -94,13 +94,12 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
    * Note: Need elasticsearch-sstmpl plugin to use this method.
    * https://github.com/codelibs/elasticsearch-sstmpl
    */
-  def searchByTemplateAsync(config: ESConfig)(lang: String, template: String, params: AnyRef, options: Option[String] = None): Future[Either[Map[String, Any], Map[String, Any]]] = {
+  def searchByTemplateAsync(config: ESConfig)(template: String, params: AnyRef, options: Option[String] = None): Future[Either[Map[String, Any], Map[String, Any]]] = {
     if(scriptTemplateIsAvailable) {
       logger.debug("******** ESConfig:" + config.toString)
       val json = JsonUtils.serialize(
         Map(
-          "lang" -> lang,
-          "file" -> template,
+          "id" -> template,
           "params" -> params
         )
       )
@@ -116,7 +115,7 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def findAsync[T](config: ESConfig)(f: SearchDslBuilder => Unit)(implicit c: ClassTag[T]): Future[Option[(String, T)]] = {
+  def findAsync[T](config: ESConfig)(f: SearchSourceBuilder => Unit)(implicit c: ClassTag[T]): Future[Option[(String, T)]] = {
     searchAsync(config)(f).map {
       case Left(x)  => throw new RuntimeException(x("error").toString)
       case Right(x) => {
@@ -126,14 +125,14 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def findAsListAsync[T](config: ESConfig)(f: SearchDslBuilder => Unit)(implicit c: ClassTag[T]): Future[List[(String, T)]] = {
+  def findAsListAsync[T](config: ESConfig)(f: SearchSourceBuilder => Unit)(implicit c: ClassTag[T]): Future[List[(String, T)]] = {
     searchAsync(config)(f).map {
       case Left(x)  => throw new RuntimeException(x("error").toString)
       case Right(x) => createESSearchResult(x).list.map { x => (x.id, x.doc) }
     }
   }
 
-  def findAllAsListAsync[T](config: ESConfig)(f: SearchDslBuilder => Unit)(implicit c: ClassTag[T]): Future[List[(String, T)]] = {
+  def findAllAsListAsync[T](config: ESConfig)(f: SearchSourceBuilder => Unit)(implicit c: ClassTag[T]): Future[List[(String, T)]] = {
     countAsIntAsync(config)(f).flatMap { count =>
       findAsListAsync(config) { builder =>
         f(builder)
@@ -142,14 +141,14 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def listAsync[T](config: ESConfig)(f: SearchDslBuilder => Unit)(implicit c: ClassTag[T]): Future[ESSearchResult[T]] = {
+  def listAsync[T](config: ESConfig)(f: SearchSourceBuilder => Unit)(implicit c: ClassTag[T]): Future[ESSearchResult[T]] = {
     searchAsync(config)(f).map {
       case Left(x)  => throw new RuntimeException(x("error").toString)
       case Right(x) => createESSearchResult(x)
     }
   }
 
-  def listAllAsync[T](config: ESConfig)(f: SearchDslBuilder => Unit)(implicit c: ClassTag[T]): Future[ESSearchResult[T]] = {
+  def listAllAsync[T](config: ESConfig)(f: SearchSourceBuilder => Unit)(implicit c: ClassTag[T]): Future[ESSearchResult[T]] = {
     countAsIntAsync(config)(f).flatMap { count =>
       listAsync(config) { builder =>
         f(builder)
@@ -162,9 +161,9 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
    * Note: Need elasticsearch-sstmpl plugin to use this method.
    * https://github.com/codelibs/elasticsearch-sstmpl
    */
-  def listByTemplateAsync[T](config: ESConfig)(lang: String, template: String, params: AnyRef)(implicit c: ClassTag[T]): Future[ESSearchResult[T]] = {
+  def listByTemplateAsync[T](config: ESConfig)(template: String, params: AnyRef)(implicit c: ClassTag[T]): Future[ESSearchResult[T]] = {
     if(scriptTemplateIsAvailable){
-      searchByTemplateAsync(config)(lang, template, params).map {
+      searchByTemplateAsync(config)(template, params).map {
         case Left(x)  => throw new RuntimeException(x("error").toString)
         case Right(x) => createESSearchResult(x)
       }
@@ -176,7 +175,7 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
   def insertJsonAsync(config: ESConfig, json: String): Future[Either[Map[String, Any], Map[String, Any]]] = {
     logger.debug(s"insertJson:\n${json}")
 
-    val future = HttpUtils.postAsync(httpClient, config.url(url), json)
+    val future = HttpUtils.postAsync(httpClient, config.documentUrl(url), json)
     future.map { resultJson =>
       val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
       map.get("error").map { _ => Left(map) }.getOrElse(Right(map))
@@ -186,7 +185,7 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
   def insertJsonAsync(config: ESConfig, id: String, json: String): Future[Either[Map[String, Any], Map[String, Any]]] = {
     logger.debug(s"insertJson:\n${json}")
 
-    val future = HttpUtils.postAsync(httpClient, config.url(url) + "/" + id, json)
+    val future = HttpUtils.putAsync(httpClient, config.documentUrl(url) + "/" + id, json)
     future.map { resultJson =>
       val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
       map.get("error").map { _ => Left(map) }.getOrElse(Right(map))
@@ -204,7 +203,7 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
   def updateJsonAsync(config: ESConfig, id: String, json: String): Future[Either[Map[String, Any], Map[String, Any]]] = {
     logger.debug(s"updateJson:\n${json}")
 
-    val future = HttpUtils.putAsync(httpClient, config.url(url) + "/" + id, json)
+    val future = HttpUtils.putAsync(httpClient, config.documentUrl(url) + "/" + id, json)
     future.map { resultJson =>
       val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
       map.get("error").map { _ => Left(map) }.getOrElse(Right(map))
@@ -218,20 +217,32 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
   def deleteAsync(config: ESConfig, id: String): Future[Either[Map[String, Any], Map[String, Any]]] = {
     logger.debug(s"delete id:\n${id}")
 
-    val future = HttpUtils.deleteAsync(httpClient, config.url(url) + "/" + id)
+    val future = HttpUtils.deleteAsync(httpClient, config.documentUrl(url) + "/" + id)
     future.map { resultJson =>
       val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
       map.get("error").map { _ => Left(map) }.getOrElse(Right(map))
     }
   }
 
-  //def deleteByQueryAsync(config: ESConfig)(f: SearchRequestBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = ???
-
-  def countAsync(config: ESConfig)(f: SearchDslBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = {
+  def deleteByQueryAsync(config: ESConfig)(f: SearchSourceBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = {
     logger.debug("******** ESConfig:" + config.toString)
-    val builder = SearchDslBuilder.builder()
+    val builder = new SearchSourceBuilder()
     f(builder)
-    val json = builder.build()
+    val json = builder.toString
+    logger.debug(s"deleteByQuery:${json}")
+
+    val future = HttpUtils.postAsync(httpClient, config.url(url) + "/_delete_by_query", json)
+    future.map { resultJson =>
+      val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
+      map.get("error").map { _ => Left(map) }.getOrElse(Right(map))
+    }
+  }
+
+  def countAsync(config: ESConfig)(f: SearchSourceBuilder => Unit): Future[Either[Map[String, Any], Map[String, Any]]] = {
+    logger.debug("******** ESConfig:" + config.toString)
+    val builder = new SearchSourceBuilder()
+    f(builder)
+    val json = builder.toString
     logger.debug(s"countRequest:${json}")
 
     val future = HttpUtils.postAsync(httpClient, config.preferenceUrl(url, "_count"), json)
@@ -241,7 +252,7 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def countAsIntAsync(config: ESConfig)(f: SearchDslBuilder => Unit): Future[Int] = {
+  def countAsIntAsync(config: ESConfig)(f: SearchSourceBuilder => Unit): Future[Int] = {
     countAsync(config)(f).map {
       case Left(x)  => throw new RuntimeException(x("error").toString)
       case Right(x) => x("count").asInstanceOf[Int]
@@ -252,9 +263,9 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
    * Note: Need elasticsearch-sstmpl plugin to use this method.
    * https://github.com/codelibs/elasticsearch-sstmpl
    */
-  def countByTemplateAsync(config: ESConfig)(lang: String, template: String, params: AnyRef): Future[Either[Map[String, Any], Map[String, Any]]] = {
+  def countByTemplateAsync(config: ESConfig)(template: String, params: AnyRef): Future[Either[Map[String, Any], Map[String, Any]]] = {
     if(scriptTemplateIsAvailable){
-      searchByTemplateAsync(config)(lang, template, params, Some("?search_type=query_then_fetch&size=0"))
+      searchByTemplateAsync(config)(template, params, Some("?search_type=query_then_fetch&size=0"))
     } else {
       throw new UnsupportedOperationException("You can install elasticsearch-sstmpl plugin to use this method.")
     }
@@ -264,9 +275,9 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
    * Note: Need elasticsearch-sstmpl plugin to use this method.
    * https://github.com/codelibs/elasticsearch-sstmpl
    */
-  def countByTemplateAsIntAsync(config: ESConfig)(lang: String, template: String, params: AnyRef): Future[Int] = {
+  def countByTemplateAsIntAsync(config: ESConfig)(template: String, params: AnyRef): Future[Int] = {
     if(scriptTemplateIsAvailable){
-      countByTemplateAsync(config)(lang, template, params).map {
+      countByTemplateAsync(config)(template, params).map {
         case Left(x)  => throw new RuntimeException(x("error").toString)
         case Right(x) => x("hits").asInstanceOf[Map[String, Any]]("total").asInstanceOf[Int]
       }
@@ -275,7 +286,7 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def refreshAsync(config: ESConfig)(): Future[Either[Map[String, Any], Map[String, Any]]] = {
+  def refreshAsync(config: ESConfig): Future[Either[Map[String, Any], Map[String, Any]]] = {
     val future = HttpUtils.postAsync(httpClient, s"${url}/${config.indexName}/_refresh", "")
     future.map { resultJson =>
       val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
@@ -290,11 +301,11 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def scrollAsync[T, R](config: ESConfig)(f: SearchDslBuilder => Unit)(p: (String, T) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
+  def scrollAsync[T, R](config: ESConfig)(f: SearchSourceBuilder => Unit)(p: (String, T) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
     logger.debug("******** ESConfig:" + config.toString)
-    val builder = SearchDslBuilder.builder()
+    val builder = new SearchSourceBuilder()
     f(builder)
-    val json = builder.build()
+    val json = builder.toString
     logger.debug(s"searchRequest:${json}")
 
     _scroll0(true, config.url(url) + "/_search", json, Stream.empty,
@@ -305,13 +316,12 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
    * Note: Need elasticsearch-sstmpl plugin to use this method.
    * https://github.com/codelibs/elasticsearch-sstmpl
    */
-  def scrollByTemplateAsync[T, R](config: ESConfig)(lang: String, template: String, params: AnyRef)(p: (String, T) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
+  def scrollByTemplateAsync[T, R](config: ESConfig)(template: String, params: AnyRef)(p: (String, T) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
     if(scriptTemplateIsAvailable) {
       logger.debug("******** ESConfig:" + config.toString)
       val json = JsonUtils.serialize(
         Map(
-          "lang" -> lang,
-          "file" -> template,
+          "id" -> template,
           "params" -> params
         )
       )
@@ -346,11 +356,11 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
     }
   }
 
-  def scrollChunkAsync[T, R](config: ESConfig)(f: SearchDslBuilder => Unit)(p: (Seq[(String, T)]) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
+  def scrollChunkAsync[T, R](config: ESConfig)(f: SearchSourceBuilder => Unit)(p: (Seq[(String, T)]) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
     logger.debug("******** ESConfig:" + config.toString)
-    val builder = SearchDslBuilder.builder()
+    val builder = new SearchSourceBuilder()
     f(builder)
-    val json = builder.build()
+    val json = builder.toString
     logger.debug(s"searchRequest:${json}")
 
     _scrollChunk0(true, config.url(url) + "/_search", json, Stream.empty,
@@ -364,13 +374,12 @@ class AsyncESClient(httpClient: AsyncHttpClient, url: String, scriptTemplateIsAv
    * Note: Need elasticsearch-sstmpl plugin to use this method.
    * https://github.com/codelibs/elasticsearch-sstmpl
    */
-  def scrollChunkByTemplateAsync[T, R](config: ESConfig)(lang: String, template: String, params: AnyRef)(p: (Seq[(String, T)]) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
+  def scrollChunkByTemplateAsync[T, R](config: ESConfig)(template: String, params: AnyRef)(p: (Seq[(String, T)]) => R)(implicit c1: ClassTag[T], c2: ClassTag[R]): Future[Stream[R]] = {
     if(scriptTemplateIsAvailable) {
       logger.debug("******** ESConfig:" + config.toString)
       val json = JsonUtils.serialize(
         Map(
-          "lang" -> lang,
-          "file" -> template,
+          "id" -> template,
           "params" -> params
         )
       )
